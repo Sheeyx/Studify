b1f5aa5b084a00b18d1481d635326154159c83bc
Sheeyx <sheeyx00@gmail.com>
2024-10-28 12:41:18 +0900

    fix: develop responsive Hero Section

--- src/screens/home/HeroSection/index.tsx
+++ src/screens/home/HeroSection/index.tsx
@@ -1,46 +1,56 @@
-import React from 'react'
+import React from 'react';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import "./styles.scss"
-import Photo from "../../../assets/uni-photo.jpeg"
-import Img1 from "../../../assets/hero-section/glasgow.png"
-import Img2 from "../../../assets/hero-section/harvard-university.png"
-import Img3 from "../../../assets/hero-section/university-sydney.png"
+import { Container, Grid, Button, Typography, Box } from '@mui/material';
+import "./styles.scss";
+import Photo from "../../../assets/uni-photo.jpeg";
+import Img1 from "../../../assets/hero-section/glasgow.png";
+import Img2 from "../../../assets/hero-section/harvard-university.png";
+import Img3 from "../../../assets/hero-section/university-sydney.png";
 
 export default function HeroSection() {
   return (
-    <div className='hero-section'>
-        <div className='container'>
-            <div className='hero-left'>
-                <h2>
-                    Your dream of studying abroad is our mission
-                </h2>
-                <p>We know how to speed up the application process and help you save time and money.</p>
-                <button>Free consultation <ArrowForwardIcon /></button>
-            </div>
-            <div className='hero-right'>
-                <div className='image'>
-                    <img src={Photo} alt="" />
-                </div>
-                <div className='percentage'>
-                    <div className='box'>
-                        <p className='number'>5+</p>
-                        <p>Success rate</p>
-                    </div>
-                    <div className='box-circle'>
-                        <p className='number'>99%</p>
-                        <p>Success rate</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className='partner container'>
-            <div className='text'>Partnered with top universities</div>
-            <div className='img-box'>
-                <img src={Img1} alt="" />
-                <img src={Img2} alt="" />
-                <img src={Img3} alt="" />
-            </div>
-        </div>
-    </div>
-  )
+    <Box className="hero-section container">
+        <Grid container spacing={4} alignItems="center" mt={6}>
+          <Grid item xs={12}  md={6} className="hero-left">
+            <Typography variant="h2" component="h2">
+              Your dream of studying abroad is our mission
+            </Typography>
+            <Typography variant="body1" className="hero-description">
+              We know how to speed up the application process and help you save time and money.
+            </Typography>
+            <Button variant="contained" className="hero-button" endIcon={<ArrowForwardIcon />}>
+              Free consultation
+            </Button>
+          </Grid>
+
+          <Grid item xs={12} md={6} className="hero-right">
+            <Box className="image">
+              <img src={Photo} alt="University" />
+            </Box>
+            <Box className="percentage">
+              <Box className="box">
+                <Typography className="number">5+</Typography>
+                <Typography>Years of Experience</Typography>
+              </Box>
+              <Box className="box-circle">
+                <Typography className="number">99%</Typography>
+                <Typography>Success rate</Typography>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+
+        {/* Partner Universities Section */}
+        <Grid container className="partner" spacing={4} alignItems="center">
+          <Grid item xs={12} md={3}>
+            <Typography className="text">Partnered with top universities</Typography>
+          </Grid>
+          <Grid item xs={12} md={9} className="img-box">
+            <img src={Img1} alt="University of Glasgow" />
+            <img src={Img2} alt="Harvard University" />
+            <img src={Img3} alt="University of Sydney" />
+          </Grid>
+        </Grid>
+    </Box>
+  );
 }
