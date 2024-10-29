7892c381fb0f4e846d0b287e368d1f65c7e36b4c
michael <abdulazizklichev1@gmail.com>
2024-10-29 11:13:22 +0900

    fix:modify changePassword component

--- src/screens/admin/MyPage/index.tsx
+++ src/screens/admin/MyPage/index.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import MemberUpdateForm from "../components/MemberUpdate/MemberUpdate";
 import ChangePasswordForm from "../components/ChangePassword/ChangePassword";
 
+const MyPage: React.FC = () => {
+  const [showChangePassword, setShowChangePassword] = useState(false);
 
-const MyPage: React.FC = () => (
-  <div>
-    <h2>My Page</h2>
-    <ChangePasswordForm/>
-    <MemberUpdateForm />
+  const toggleChangePassword = () => setShowChangePassword(!showChangePassword);
 
-  </div>
-);
+  return (
+    <div>
+      <h2 style={{ marginTop: "10px" }}>My Page</h2>
+      <button
+        onClick={toggleChangePassword}
+        style={{
+          width: "150px", // You can set this to a smaller width as desired
+          padding: "8px 1px",
+        }}
+      >
+        {showChangePassword ? "Back to Profile" : "Change Password"}
+      </button>
+      {showChangePassword ? (
+        <ChangePasswordForm />
+      ) : (
+        <MemberUpdateForm />
+      )}
+    </div>
+  );
+};
 
 export default MyPage;
