bd2c1d320ec237f2b8135bd149ae3352a19734f3
Sheeyx <sheeyx00@gmail.com>
2024-11-11 04:08:46 +0900

    fix: develop responsive of ContactUs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -41,7 +41,7 @@ function App() {
               <PricePackage />
               <OurResults />
               <CustomerReview />
-              {/* <ContactUs /> */}
+              <ContactUs />
               {/* <FAQ /> */}
               {/* <Footer /> */}
             </>
