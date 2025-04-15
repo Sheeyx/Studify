79a330e5fa69026f192641c9cd6bed97cfa8d76c
Sheeyx <sheeyx00@gmail.com>
2025-04-15 09:53:10 +0900

    fix: change header to fixed position

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -26,7 +26,7 @@ const Header = () => {
   
 
   return (
-    <header className="header container">
+    <header className="header">
       <div className="logo">
         <img src={Logo} alt="Studify logo" />
       </div>
